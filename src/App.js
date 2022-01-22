e3e0dfabd65e8356eb782de86765a224e060ca0d
Evan Thome <evancthome@gmail.com>
2022-01-22 15:45:22 -0500

    add json server and fetch from server

--- src/App.js
+++ src/App.js
@@ -1,67 +1,29 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
-import Tasks from './components/Tasks'
-import AddTask from './components/AddTask'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import About from './components/About'
 
 function App() {
   const [showAddTasks, setShowAddTasks] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Zoom meeting',
-      day: 'March 5th at 4:30 PM',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Chair being delivered',
-      day: 'March 6th at 5:30 PM',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Late lunch with co-workers',
-      day: 'March 8th at 2:30 PM',
-      reminder: false,
-    },
-  ])
-
-  //Delete a task
-  const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id))
-  }
-
-  //Toggle reminder
-  const toggleReminder = (id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task,
-      ),
-    )
-  }
-
-  //Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
-  }
   return (
-    <div className='container'>
-      <Header
-        onAdd={() => setShowAddTasks(!showAddTasks)}
-        showAdd={showAddTasks}
-        title='Task Tracker'
-      />
-      {showAddTasks && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : (
-        'No tasks'
-      )}
-    </div>
+    <Router>
+      <div className='container'>
+        <Header
+          onAdd={() => setShowAddTasks(!showAddTasks)}
+          showAdd={showAddTasks}
+          title='Task Tracker'
+        />
+
+        <Routes>
+          <Route path='/about' element={<About />} />
+          <Route path='/' element={<Main />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
